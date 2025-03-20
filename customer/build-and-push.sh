118c4367eacf4aae550046a2794744867a19d93d
Maarten Schenkeveld <maschenkeveld@gmail.com>
2025-03-20 13:15:44 +0100

    Maarten

--- customer/build-and-push.sh
+++
@@ -1,8 +0,0 @@
-BUILD_VERSION="1.0.0"
-IMAGE_NAME="kong-air-sales-customer"
-
-docker buildx build --platform linux/amd64 -t "nexus.pve-1.schenkeveld.io:8445/${IMAGE_NAME}:${BUILD_VERSION}" -f Dockerfile .
-
-docker login nexus.pve-1.schenkeveld.io:8445 -u admin -p haqxDpyly6iSmLrgNpDt
-
-docker push "nexus.pve-1.schenkeveld.io:8445/${IMAGE_NAME}:${BUILD_VERSION}"
